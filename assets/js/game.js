b77539954d1f7736d687a61ef3469263103f8a56
Sean Gilleece <seangilleece@gmail.com>
2021-06-22 20:59:12 +0100

    added drawPaddle function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,17 +1,31 @@
-// Gets the Canvas that the game is played on
+// Gets the canvas that the game is played on
 const playSpace = document.getElementById("game");
 const context = playSpace.getContext("2d");
 
-// Game Variables and Constants
+// Game variables and constants
 const PADDLE_WIDTH = 150;
 const PADDLE_MARGIN_BOTTOM = 75;
 const PADDLE_HEIGHT = 20;
 
-// Create Paddle
+// Sets Line Width thicker for visibility
+context.lineWidth = 3;
+
+// Create the paddle
 const paddle = {
-    x : cvs.width/2 - PADDLE_WIDTH/2,
-    y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    x : playSpace.width/2 - PADDLE_WIDTH/2,
+    y : playSpace.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
     width : PADDLE_WIDTH,
     height : PADDLE_HEIGHT,
     dx :5
 }
+
+// Draw the paddle
+function drawPaddle(){
+    context.fillStyle = "#2e3548";
+    context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    
+    context.strokeStyle = "#ffcd05";
+    context.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+drawPaddle();
