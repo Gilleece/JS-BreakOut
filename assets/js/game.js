357ad3b8441c9558403c8df4ecfa0feaad11f146
Sean Gilleece <seangilleece@gmail.com>
2021-06-22 20:49:14 +0100

    Added paddle and required variables and constants

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,3 +1,17 @@
 // Gets the Canvas that the game is played on
 const playSpace = document.getElementById("game");
 const context = playSpace.getContext("2d");
+
+// Game Variables and Constants
+const PADDLE_WIDTH = 150;
+const PADDLE_MARGIN_BOTTOM = 75;
+const PADDLE_HEIGHT = 20;
+
+// Create Paddle
+const paddle = {
+    x : cvs.width/2 - PADDLE_WIDTH/2,
+    y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width : PADDLE_WIDTH,
+    height : PADDLE_HEIGHT,
+    dx :5
+}
