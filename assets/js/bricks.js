81cf43b8af521892847877a68183f70c76c2812b
Sean Gilleece <seangilleece@gmail.com>
2021-06-23 19:00:26 +0100

    Began adding brick logic

---
+++ assets/js/bricks.js
@@ -0,0 +1,14 @@
+// Creates the bricks
+const brick = {
+    width : 55,
+    height : 20,
+    offSetLeft : 20,
+    offSetTop : 20,
+    marginTop : 40,
+    row : 1,
+    column : 5,
+    fillColor : "#2e3548",
+    strokeColor : "#FFF"
+}
+
+let bricks = [];
